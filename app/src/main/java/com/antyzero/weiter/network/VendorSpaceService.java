77d1d8442688be6ce3defe9eb23040c46a253abc
outlying <outlying@gmail.com>
2014-10-17 23:36:13 +0200

    WIP

--- app/src/main/java/com/antyzero/weiter/network/VendorSpaceService.java
+++ app/src/main/java/com/antyzero/weiter/network/VendorSpaceService.java
@@ -1,8 +1,17 @@
 package com.antyzero.weiter.network;
 
+import com.antyzero.weiter.network.model.Vendor;
+
+import java.util.List;
+
+import retrofit.http.GET;
+import retrofit.http.Query;
+
 /**
  * ...
  */
 public interface VendorSpaceService {
 
+    @GET( "/vendors" )
+    List<Vendor> listVendors( @Query("lng") int lng, @Query( "lat" ) int lat );
 }
---
+++ app/src/main/java/com/antyzero/weiter/network/model/Vendor.java
@@ -0,0 +1,9 @@
+package com.antyzero.weiter.network.model;
+
+/**
+ * Created by tornax on 17.10.14.
+ */
+public class Vendor {
+
+    
+}
