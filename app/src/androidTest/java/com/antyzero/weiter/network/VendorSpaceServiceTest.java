46e2b008547713b6c88a7a9c0c04254b73d6bc43
outlying <outlying@gmail.com>
2014-10-18 01:16:41 +0200

    Add test

---
+++ app/src/androidTest/java/com/antyzero/weiter/network/VendorSpaceServiceTest.java
@@ -0,0 +1,18 @@
+package com.antyzero.weiter.network;
+
+import junit.framework.TestCase;
+
+public class VendorSpaceServiceTest extends TestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+
+    }
+
+    public void testVendor() throws Exception {
+
+
+    }
+}
