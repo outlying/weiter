d42e39b8509a7d1ef1448a632df5f682c69dbd10
outlying <outlying@gmail.com>
2014-10-18 01:07:31 +0200

    Adding vendor activity class

---
+++ app/src/main/java/com/antyzero/weiter/ui/VendorActivity.java
@@ -0,0 +1,17 @@
+package com.antyzero.weiter.ui;
+
+import android.os.Bundle;
+
+import com.antyzero.weiter.R;
+
+/**
+ * Created by tornax on 18.10.14.
+ */
+public class VendorActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_vendor );
+    }
+}
