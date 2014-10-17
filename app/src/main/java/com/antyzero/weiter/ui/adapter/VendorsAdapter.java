8474e8c954e7c7d2562ad0ccaf4d432991493303
outlying <outlying@gmail.com>
2014-10-18 00:36:36 +0200

    Added vendor adapter

--- app/src/main/java/com/antyzero/weiter/ui/adapter/VendorsAdapter.java
+++ app/src/main/java/com/antyzero/weiter/ui/adapter/VendorsAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.antyzero.weiter.R;
 import com.antyzero.weiter.network.model.Vendor;
 
 import java.util.List;
@@ -18,9 +21,9 @@ public class VendorsAdapter extends BaseAdapter {
     private final List<Vendor> vendorList;
     private final LayoutInflater layoutInflater;
 
-    public VendorsAdapter(Context context, List<Vendor> vendorList) {
+    public VendorsAdapter( Context context, List<Vendor> vendorList ) {
 
-        if(vendorList == null){
+        if( vendorList == null ) {
             throw new IllegalArgumentException( "List cannot be null" );
         }
 
@@ -46,8 +49,41 @@ public class VendorsAdapter extends BaseAdapter {
     @Override
     public View getView( int position, View convertView, ViewGroup parent ) {
 
-        // TODO ...
+        ViewHolder viewHolder;
 
-        return null;
+        if(convertView == null){
+            convertView = layoutInflater.inflate( R.layout.adapter_vendor, parent, false );
+
+            viewHolder = new ViewHolder( convertView );
+
+            convertView.setTag( viewHolder );
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        Vendor vendor = getItem( position );
+
+        viewHolder.textViewProducts.setText( "ASD" ); // TODO
+        viewHolder.textViewTitle.setText( vendor.getName() );
+
+        // TODO image loading
+
+        return convertView;
+    }
+
+    /**
+     * ViewHolder pattern
+     */
+    private static class ViewHolder {
+
+        private final TextView textViewTitle;
+        private final TextView textViewProducts;
+        private final ImageView imageView;
+
+        private ViewHolder( View view ) {
+            textViewTitle = (TextView) view.findViewById( R.id.textViewTitle );
+            textViewProducts = (TextView) view.findViewById( R.id.textViewProducts );
+            imageView = (ImageView) view.findViewById( R.id.imageView );
+        }
     }
 }
